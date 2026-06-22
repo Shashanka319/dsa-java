628e7eb566ab94dd2bb0b2f3311ec77a8eb52a21
Shashank K <sharathshashank1986@gmail.com>
2026-06-22 21:06:23 +0530

    Lowest Element

--- DSA_Problems/src/main/java/com/dsa/arrays/LowestElement.java
+++ DSA_Problems/src/main/java/com/dsa/arrays/LowestElement.java
@@ -1,13 +1,17 @@
 package com.dsa.arrays;
 
 public class LowestElement {
-    pulic static int lowestElement(int[] arr){
+    public static int lowestElement(int[] arr){
         int min=arr[0];
-        for(int i=0;i<arr.lenght;i++){
+        for(int i=0;i<arr.length;i++){
             if(arr[i]<min){
                 min=arr[i];
             }
         }
         return min;
+    }
+    public static void main(String[] args){
+        int[] numbers={56,93,2,3,85,929};
+        System.out.println("Lowest Element in Given Array:"+lowestElement(numbers));
     }
 }
