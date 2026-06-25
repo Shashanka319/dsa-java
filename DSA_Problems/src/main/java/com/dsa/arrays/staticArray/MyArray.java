58b82071fb2038dc97b76a4fa4c2b8ce7f4f1149
Shashank K <sharathshashank1986@gmail.com>
2026-06-25 07:20:06 +0530

    initialize the array

---
+++ DSA_Problems/src/main/java/com/dsa/arrays/staticArray/MyArray.java
@@ -0,0 +1,13 @@
+package com.dsa.arrays.staticArray;
+
+
+import java.util.Arrays;
+
+public class MyArray {
+    public static void main(String[] args){
+        int[] myArray={1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Array Elements:"+ Arrays.toString(myArray));
+    }
+
+
+}
