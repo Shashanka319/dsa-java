a87bb06b1768056a87b7aaa99e09b783b98046c3
Shashank K <sharathshashank1986@gmail.com>
2026-06-25 07:36:56 +0530

    read Operation

--- DSA_Problems/src/main/java/com/dsa/arrays/staticArray/MyArray.java
+++ DSA_Problems/src/main/java/com/dsa/arrays/staticArray/MyArray.java
@@ -5,9 +5,34 @@ import java.util.Arrays;
 
 public class MyArray {
     public static void main(String[] args){
-        int[] myArray={1,2,3,4,5,6,7,8,9,10};
-        System.out.println("Array Elements:"+ Arrays.toString(myArray));
+        int[] myArray=new int[10];
+        myArray[0]=1;
+        myArray[1]=2;
+        myArray[2]=3;
+        myArray[3]=4;
+        myArray[4]=5;
+        myArray[5]=6;
+        myArray[6]=7;
+        myArray[7]=8;
+        myArray[8]=9;
+        myArray[9]=10;
+        System.out.println("Total Array elements:"+Arrays.toString(myArray));
+
+        System.out.println("Array Element at index 0:"+myArray[0]);
+        System.out.println("Array Element at index 1:"+myArray[1]);
+        System.out.println("Array Element at index 2:"+myArray[2]);
+        System.out.println("Array Element at index 3:"+myArray[3]);
+        System.out.println("Array Element at index 4:"+myArray[4]);
+        System.out.println("Array Element at index 5:"+myArray[5]);
+        System.out.println("Array Element at index 6:"+myArray[6]);
+        System.out.println("Array Element at index 7:"+myArray[7]);
+        System.out.println("Array Element at index 8:"+myArray[8]);
+        System.out.println("Array Element at index 9:"+myArray[9]);
+
+
+
     }
+
 
 
 }
