ff16cb9a9d7a5edef96d450aeb0aa0eb7e801e0c
Shashank K <sharathshashank1986@gmail.com>
2026-06-26 20:59:07 +0530

    wihtout comments

--- DSA_Problems/src/main/java/com/dsa/arrays/staticArray/Rotate.java
+++ DSA_Problems/src/main/java/com/dsa/arrays/staticArray/Rotate.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 public class Rotate {
     public static void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n; // Handles cases where k is greater than array length
+        k = k % n;
 
-        // Step 1: Reverse the whole array
+
         reverseHelper(nums, 0, n - 1);
 
-        // Step 2: Reverse the first k elements
+        /
         reverseHelper(nums, 0, k - 1);
 
-        // Step 3: Reverse the remaining elements
+
         reverseHelper(nums, k, n - 1);
     }
 
-    // A custom helper function to reverse a specific section of an array
+
     private static void reverseHelper(int[] arr, int start, int end) {
         while (start < end) {
             int temp = arr[start];
@@ -37,7 +37,7 @@ public class Rotate {
         System.out.println("K value of Given Rattating COndition:"+k);
 
         for (int num : arr) {
-            System.out.print("Rotated Array Elements:"+num + " "); // Output: 5 6 7 1 2 3 4
+            System.out.print("Rotated Array Elements:"+num + " "); 
         }
     }
 }
